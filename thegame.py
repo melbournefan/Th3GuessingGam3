b0cc0145975deb915d286c5a288742933b878e37
Matt D <matthewdavalle1@gmail.com>
2020-01-10 17:25:47 +1100

    reranged imports, added time import, added sleep command

--- thegame.py
+++ thegame.py
@@ -1,8 +1,10 @@
-from random import seed
 from asciimatics.screen import Screen
+from random import seed
+from time import sleep
 
 def mainscreen(screen):
-    screen.print_at('Th3GuessingGam3', 0, 0)
+    screen.print_at('Th3GuessingGam3', 0, 0,)
     screen.refresh()
+    sleep(10)
 
 Screen.wrapper(mainscreen)
