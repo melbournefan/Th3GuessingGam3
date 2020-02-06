08b336e4bff9eb908530fcf8a47b50538f82071e
melbournefan <41057813+melbournefan@users.noreply.github.com>
2020-02-06 15:00:25 +1100

    fixed errors

--- thegame.py
+++ thegame.py
@@ -4,7 +4,7 @@ rdmb = random.randint(1, 20)
 answr = "S"
 print ('Welcome to Tha3GuessingGam3, press the S key to start')
 
-while = "S"
+while answr == "S":
     gss = input("Type a number from 1 to 20, remember if you want to exit you can press the 'H' Key.")
-    if gss == 'H'
-    break
+    if gss == 'H':
+        break
