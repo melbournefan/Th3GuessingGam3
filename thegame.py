8154296a4e0c5ad3bcd1cbf00aba9aba47bdeca1
melbournefan <41057813+melbournefan@users.noreply.github.com>
2020-02-06 14:59:30 +1100

    edited error, changed S key to H for force exit

--- thegame.py
+++ thegame.py
@@ -2,9 +2,9 @@ import random
 
 rdmb = random.randint(1, 20)
 answr = "S"
-print 'Welcome to Tha3GuessingGam3, press the S key to start'
+print ('Welcome to Tha3GuessingGam3, press the S key to start')
 
 while = "S"
     gss = input("Type a number from 1 to 20, remember if you want to exit you can press the 'H' Key.")
-    if gss == 'S'
+    if gss == 'H'
     break
