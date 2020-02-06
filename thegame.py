5da601f8d91525dfecde8942a1842013384b49ea
melbournefan <41057813+melbournefan@users.noreply.github.com>
2020-02-06 14:55:27 +1100

    added starting code in

--- thegame.py
+++ thegame.py
@@ -1,3 +1,8 @@
-from random import seed
+import random
 
+rdmb = random.randint(1, 20)
+answr = "S"
 print 'Welcome to Tha3GuessingGam3, press the S key to start'
+
+while = "S"
+    print("Type a number from 1 to 20")
