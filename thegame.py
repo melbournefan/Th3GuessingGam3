c489361d811e4c40507b8ad2ce670687765a1cec
melbournefan <41057813+melbournefan@users.noreply.github.com>
2020-02-06 15:09:47 +1100

    added correct answer, changed highest from 20 to 10

--- thegame.py
+++ thegame.py
@@ -1,9 +1,9 @@
 import random
 
-rdmb = random.randint(1, 20)
+rdmb = random.randint(1, 10)
 answr = "S"
 while answr == "S":
-    gss = input("Welcome to Tha3GuessingGam3. Type a number from 1 to 20, remember if you want to exit you can press the 'H' Key \n")
+    gss = input("Welcome to Tha3GuessingGam3. Type a number from 1 to 10, remember if you want to exit you can press the 'H' Key \n")
     if gss == 'H':
         print("Thanks 4 playing, goodbai")
         break
@@ -13,3 +13,6 @@ while answr == "S":
 
     elif int(gss) < rdmb:
         print("Worng, please input a HIGHER number")
+
+    elif int(gss) == rdmb:
+        print("WINNER. You won the JackPOT LMAO")
