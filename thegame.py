f3c07ebb25b78529564d24bf6920fab20669693b
melbournefan <41057813+melbournefan@users.noreply.github.com>
2020-02-06 15:07:52 +1100

    most operational code up and running

--- thegame.py
+++ thegame.py
@@ -5,7 +5,11 @@ answr = "S"
 while answr == "S":
     gss = input("Welcome to Tha3GuessingGam3. Type a number from 1 to 20, remember if you want to exit you can press the 'H' Key \n")
     if gss == 'H':
+        print("Thanks 4 playing, goodbai")
         break
 
     elif int(gss) > rdmb:
         print("Worng, please input a LOWER number")
+
+    elif int(gss) < rdmb:
+        print("Worng, please input a HIGHER number")
