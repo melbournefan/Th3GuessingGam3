6128488ea8e6b189edefcc6c605cf184771b2164
Matthew DAVALLE <DAV0005@mountalexandercollege.vic.edu.au>
2020-02-07 12:16:43 +1100

    fixed definations around

--- thegame.py
+++ thegame.py
@@ -3,7 +3,7 @@ import random
 rdmb = random.randint(1, 10)
 answr = "S"
 ext = "H"
-print("Welcome to Tha3GuessingGam3. Press the 'S' key to contiune \n")
+print("Welcome to Tha3GuessingGam3. Press")
 while answr == "S":
     input("Type a number from 1 to 10, remember if you want to exit you can press the 'H' Key \n")
     if ext == 'H':
