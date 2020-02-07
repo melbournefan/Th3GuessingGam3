fcd74bafd4dc116d0234f6758ecbf5eeaa0666dd
Matthew DAVALLE <DAV0005@mountalexandercollege.vic.edu.au>
2020-02-07 12:26:59 +1100

    tried to fix break bug

--- thegame.py
+++ thegame.py
@@ -1,15 +1,14 @@
 import random
 
 rdmb = random.randint(1, 10)
-gss = "S"
+gss = "P"
 ext = "H"
-print("Welcome to Tha3GuessingGam3. Press")
-while gss == "S":
+print("Welcome to Tha3GuessingGam3. Press the 'P' key to start playing")
+while gss == "P":
     input("Type a number from 1 to 10, remember if you want to exit you can press the 'H' Key \n")
-    if ext == 'H':
+    if ext == "H":
         print("Thanks 4 playing, goodbai")
         break
-
     elif int(gss) > rdmb:
         print("Worng, please input a LOWER number")
 
@@ -18,3 +17,5 @@ while gss == "S":
 
     elif int(gss) == rdmb:
         print("WINNER. You won the JackPOT LMAO")
+    else:
+        print("test")
