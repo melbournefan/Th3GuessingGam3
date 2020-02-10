8c6e5fc78aae91035d35d021fff2cd6bc99878f4
melbournefan <41057813+melbournefan@users.noreply.github.com>
2020-02-11 09:45:24 +1100

    added code dictonary, fixed troubled code that caused forced exit

--- thegame.py
+++ thegame.py
@@ -3,9 +3,9 @@ import random
 rdmb = random.randint(1, 10)
 gss = "P"
 ext = "H"
-print("Welcome to Tha3GuessingGam3. Press the 'P' key to start playing")
+input("Welcome to Tha3GuessingGam3. Press the 'P' key to start playing")
 while gss == "P":
-    input("Type a number from 1 to 10, remember if you want to exit you can press the 'H' Key \n")
+    ext = input("Type a number from 1 to 10, remember if you want to exit you can press the 'H' Key \n")
     if ext == "H":
         print("Thanks 4 playing, goodbai")
         break
@@ -19,3 +19,13 @@ while gss == "P":
         print("WINNER. You won the JackPOT LMAO")
     else:
         print("test")
+
+
+
+
+        ##DATA Dictonary
+        ## import = import a library, this can be a 'code helper' that imports code to help with the code
+        ## rdmb, gss, ext = dictonary functions, these define a word to then later put in the code later, in this case rdmb points to a function that points to the random library
+        ## gss and ext point to a keyboard function
+        ## print = this prints text to the screen when the program is first opened
+        ## while = a while command then 
